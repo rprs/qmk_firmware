6276d6951b6619769f4273c0d672e4c89aab9bb7
roberto <rprs@outlook.com>
2024-02-17 10:23:12 -0800

    Update mouse layer to work with trackball.
    - Changing bottom right key to trigger mouse layer.
    - Add keys so that I click things with the keyboard. The idea is to only
      move the mouse with the trackball.

--- keyboards/ergodox_ez/keymaps/rprs/keymap.c
+++ keyboards/ergodox_ez/keymaps/rprs/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| LGui |           |  \   |------+------+------+------+------+--------|
  * | LS     |   Z  |   X  |   C  |   V  |   B  |      |           | RGui |   N  |   M  |   ,  |   .  |   /  |   ]    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCtl | ~+L1 | ~+L2 |      | Apps |                                       |  Lft |  Up  |  Dn  | Rght | RGui |
+ *   | LCtl | ~+L1 | ~+L2 |      | Apps |                                       |  Lft |  Up  |  Dn  | Rght | ~+L2 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LCtl | LAlt |       | RAlt | RCtl |
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,        KC_W,      KC_E,  KC_R,   KC_T, KC_LSFT,                KC_LSFT,         KC_Y, KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,
   KC_TAB,  KC_A,        KC_S,      KC_D,  KC_F,   KC_G,                                          KC_H, KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,        KC_X,      KC_C,  KC_V,   KC_B, KC_LGUI,                RGUI_T(KC_BSLS), KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
-  KC_LCTL, TT(FNKEYS),  TT(MOUSE), KC_NO, KC_APP,                                                      KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_RGUI,
+  KC_LCTL, TT(FNKEYS),  TT(MOUSE), KC_NO, KC_APP,                                                      KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, TT(MOUSE),
                                                             KC_LCTL, KC_LALT,            KC_RALT, KC_RCTL,
                                                                      KC_HOME,            KC_PGUP,
                                                     KC_BSPC, KC_DEL, KC_END,             KC_PGDN, KC_ENT, KC_SPC
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|Trans |           |PtrScn|------+------+------+------+------+--------|
  * | Trans  |PtrScn|      |      |      |      |      |           |      |      |  F9  | F10  | F11  | F12  |  Menu  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Trans|      | LGui | LCtl | LAlt |                                       | Vol+ | Vol- | Mut  |      |      |
+ *   | Trans|      |      | LCtl | LAlt |                                       | Vol+ | Vol- | Mut  |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Trans| Trans|       | Trans| Trans|
@@ -89,16 +89,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Trans  |      |      |      |      |      |------|           |------|      | McL  | McD  | McU  | McR  |        |
  * |--------+------+------+------+------+------|Trans |           |      |------+------+------+------+------+--------|
- * | Trans  |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * | Trans  |      |      |      |      |      |      |           |      |      |      |      |      | WFwd |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Trans| LGui |      | LCtl | LAlt |                                       | Vol+ | Vol- | Mut  |      |      |
+ *   | Trans| LGui |      | LCtl | LAlt |                                       | Vol+ | Vol- | Mut  | WBck |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Trans| Trans|       | Trans| Trans|
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      | Trans|       | Trans|      |      |
+ *                                 |      |      | Trans|       |  Del |      |      |
  *                                 | Trans| Trans|------|       |------| Mb1  | Mb2  |
- *                                 |      |      | Trans|       | Trans|      |      |
+ *                                 |      |      | Trans|       |  Mb3 |      |      |
  *                                 `--------------------'       `--------------------'
  */
 [MOUSE] = LAYOUT_ergodox_pretty(
@@ -106,11 +106,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,          TT(MOUSE), KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,        KC_TRNS,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO,   KC_TRNS,
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                            KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   KC_TRNS,
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT,                                            KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_TRNS,        KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_WFWD,   KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT,                                            KC_MUTE, KC_VOLD, KC_VOLU, KC_WBAK, KC_TRNS,
                                              KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,
-                                                      KC_TRNS,        KC_TRNS,
-                                    KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_BTN1, KC_BTN2
+                                                      KC_TRNS,        KC_DEL,
+                                    KC_TRNS, KC_TRNS, KC_TRNS,        KC_BTN3, KC_BTN1, KC_BTN2
 ), }; // clang-format on
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

9cd9f787fd8e6f8ed51df1ed5498c666c6f1ec86
roberto <rprs@outlook.com>
2024-03-09 11:30:49 -0800

    Add permissive mode to make home row mods easier.
    Allows me to not have to wait the full 200 ms.

---
+++ keyboards/ergodox_ez/keymaps/rprs/config.h
@@ -0,0 +1,10 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#pragma once
+
+// For making it easier to use home row mods (it allows me to use mods wihtout
+// waiting for the tapping term).
+#define PERMISSIVE_HOLD
+
